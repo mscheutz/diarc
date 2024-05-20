7cce467d5bc779918aa8db6234f4457e641798f7
Chris Bao <chris.bao@tufts.edu>
2024-05-20 15:22:30 -0400

    robot chat gui mockup

--- gui/src/components/home/HomeScreen.tsx
+++ gui/src/components/home/HomeScreen.tsx
@@ -4,13 +4,17 @@ import React from "react";
 import Navbar from "../Navbar";
 import HeroBlock from "./HeroBlock";
 import Providers from "../Providers";
+import RobotChat from "../RobotChat";
 
 const HomeScreen = ({ navigation }) => {
     return (
-        <div className="h-screen">
+        <div className="flex flex-col justify-center items-center h-screen">
             <Providers>
                 <Navbar />
-                <HeroBlock navigation={navigation}/>
+                <div className="flex justify-center items-center \
+                h-[40rem] w-[60rem]">
+                    <RobotChat></RobotChat>
+                </div>
             </Providers>
         </div>
     );
