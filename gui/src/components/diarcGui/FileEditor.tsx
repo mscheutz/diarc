39f3ac6b72859a1046fbde0ff18f986f7083099c
Chris Bao <chris.bao@tufts.edu>
2024-06-05 11:06:39 -0400

    goal manager cleanup

--- gui/src/components/diarcGui/FileEditor.tsx
+++ gui/src/components/diarcGui/FileEditor.tsx
@@ -14,6 +14,7 @@ const FileEditor = ({ fileContents }) => {
         <Editor
             defaultLanguage="java"
             value={fileContents}
+            options={{ readOnly: true }}
         />
     );
 };
