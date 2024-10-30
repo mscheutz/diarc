5b9a7deaa8ccc093edc7ad6e4df84feeadece6ce
Marlow Fawn <marlow.fawn@gmail.com>
2024-10-30 13:04:36 -0400

    Cleaned up for push

--- core/src/main/java/edu/tufts/hrilab/rl/RLInterface.java
+++ core/src/main/java/edu/tufts/hrilab/rl/RLInterface.java
@@ -6,15 +6,33 @@ import edu.tufts.hrilab.action.justification.Justification;
 
 public interface RLInterface {
 
-    @TRADEService
-    @Action
-    public Justification callPolicy(String action);
+  /**
+   * Calls a policy created by RL.
+   *
+   * @param action Name associated with the policy
+   * @return Success/failure justification
+   */
+  @TRADEService
+  @Action
+  public Justification callPolicy(String action);
 
-    @TRADEService
-    @Action
-    public Justification learnPolicy(String action);
+  /**
+   * Creates a new policy via RL and assigns it the given name
+   *
+   * @param action Name to be associated with the new policy
+   * @return Success/failure justification
+   */
+  @TRADEService
+  @Action
+  public Justification learnPolicy(String action);
 
-    @TRADEService
-    @Action
-    public Justification updatePolicy(String failedOperator);
+  /**
+   * Updates
+   *
+   * @param failedOperator Name of the operator we want to update
+   * @return Success/failure justification
+   */
+  @TRADEService
+  @Action
+  public Justification updatePolicy(String failedOperator);
 }
